c84170e9d5bc63ed46e234664c82800899a7714e
The Nhan <luongthenhan@gmail.com>
2016-04-01 10:50:29 +0700

    add sensor ws

--- src/main/java/vn/khmt/hello/restful/HelloService.java
+++ src/main/java/vn/khmt/hello/restful/HelloService.java
@@ -1,5 +1,8 @@
 package vn.khmt.hello.restful;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,6 +12,8 @@ import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloService {
+
+    private static int id = 0;
 
     @GET
     @Path("/{name}")
@@ -22,6 +27,32 @@ public class HelloService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response helloInJSON(@PathParam("name") String name) {
         String msg = "{\"message\" : \"Hello " + name + "\"}";
+        return Response.status(Response.Status.OK).entity(msg).build();
+    }
+
+    @GET
+    @Path("/sensor/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response setTemperatureFromSensor() {
+        StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        Random r = new Random();
+        id++;
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(id);
+        sb.append(", ");
+        sb.append("\"time\": \"");
+        sb.append(sdf.format(new Date()));
+        sb.append("\", ");
+        sb.append("\"temperature\": ");
+        sb.append(r.nextFloat() * 10 + 30);
+        sb.append(", ");
+        sb.append("\"humidity\": ");
+        sb.append(r.nextFloat() * 30 + 40);
+        sb.append("}");
+        String msg = sb.toString();
+        System.out.println(msg);
         return Response.status(Response.Status.OK).entity(msg).build();
     }
 }
