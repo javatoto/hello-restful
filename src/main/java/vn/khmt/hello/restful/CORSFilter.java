69da1d653d81da478b5b704b0afa0c9dcb146709
The Nhan <luongthenhan@gmail.com>
2016-04-02 22:23:22 +0700

    sua commit #c9115e0 ko chay duoc tren heroku

--- src/main/java/vn/khmt/hello/restful/CORSFilter.java
+++ src/main/java/vn/khmt/hello/restful/CORSFilter.java
@@ -15,11 +15,11 @@ public class CORSFilter {
 
     // this method helps web services work on Glassfish
     public static Response configResponseHeader(Response response) {
-//        MultivaluedMap<String, Object> headers = response.getHeaders();
-//        headers.add("Access-Control-Allow-Origin", "*");
-//        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-//        headers.add("Access-Control-Allow-Credentials", "true");
-//        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+        MultivaluedMap<String, Object> headers = response.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
         return response;
     }
 }
